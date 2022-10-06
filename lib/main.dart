8c13677ac89207ec6a0353bac027709343c010e4
Jokerohit4 <Jokerohit4@users.noreply.github.com>
2022-10-06 16:54:43 +0530

    FILE

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,18 @@ import 'package:provider/provider.dart';
 
 import 'app/app.dart';
 
+//things that could be done better if more tine was provided
+
+/*
+this branch works perfectly but isn't very well structured
+the other branch is.Please do check that out too
+proper commenting isn't done
+provider could be used on the tabsview page
+Imagepicker cane be used to pick camera image
+test cases could be written
+structure could be btter
+push notifications could be better implemented
+*/
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
